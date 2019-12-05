dd2c47ff17b5d7de960f152f026332622e4bbfe7
Jose Miguel Zomeño Escobar <josemi2018@hotmail.com>
2019-12-05 16:27:47 +0100

    empezando3(probando yield)

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,15 +1,25 @@
-@extends('portada')
-
-@section('menu')
-<div id="header">
-    <a href="{{ url('/') }}">
-        <div class="option">Inicio</div>
-    </a>
-    <a href="{{ url('/historia') }}">
-        <div class="option">Historia</div>
-    </a>
-    <a href="{{ url('/jugadores') }}">
-        <div class="option">Jugadores</div>
-    </a>
-</div>
-@stop
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VeganFood</title>
+</head>
+<body>
+    <div id="header">
+        <a href="{{ url('/') }}">
+            <div class="option">Inicio</div>
+        </a>
+        <a href="{{ url('/historia') }}">
+            <div class="option">Historia</div>
+        </a>
+        <a href="{{ url('/jugadores') }}">
+            <div class="option">Jugadores</div>
+        </a>
+    </div>
+    <div class="contenido">
+        @yield('content')
+    </div>
+</body>
+</html>
