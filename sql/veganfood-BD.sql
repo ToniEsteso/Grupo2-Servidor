a57a55f60eb7fddafd0c9cdb051b57654bdc36fe
Toni Esteso <toniesteso97@gmail.com>
2019-12-09 10:47:07 +0100

    Actualizado archivo sql con inserts en categorias

--- sql/veganfood-BD.sql
+++ sql/veganfood-BD.sql
@@ -1,174 +1,40 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-12-2019 a las 07:50:57
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.1.32
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.6-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.2.0.5599
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE DATABASE `veganfood`;
---
--- Base de datos: `veganfood`
---
+-- Volcando datos para la tabla veganfood.categorias: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` (`id`, `nombre`, `tipo`) VALUES
+	(1, 'Carnes', 'Carnes'),
+	(2, 'Lentejas', 'Legumbres'),
+	(3, 'Lácteos', 'Lacteos');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 
--- --------------------------------------------------------
+-- Volcando datos para la tabla veganfood.direcciones: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `direcciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `direcciones` ENABLE KEYS */;
 
---
--- Estructura de tabla para la tabla `direcciones`
---
+-- Volcando datos para la tabla veganfood.informacionnutricional: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `informacionnutricional` DISABLE KEYS */;
+/*!40000 ALTER TABLE `informacionnutricional` ENABLE KEYS */;
 
-CREATE TABLE `direcciones` (
-  `id` int(11) NOT NULL,
-  `idUsuario` int(11) NOT NULL,
-  `codigoPostal` int(5) NOT NULL,
-  `localidad` varchar(40) NOT NULL,
-  `direccion` varchar(60) NOT NULL,
-  `numero` int(3) NOT NULL,
-  `piso` int(3) NOT NULL,
-  `escalera` varchar(2) NOT NULL,
-  `puerta` int(3) NOT NULL,
-  `masInfo` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Volcando datos para la tabla veganfood.productos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
--- --------------------------------------------------------
+-- Volcando datos para la tabla veganfood.usuarios: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
---
--- Estructura de tabla para la tabla `informacionnutricional`
---
-
-CREATE TABLE `informacionnutricional` (
-  `id` int(11) NOT NULL,
-  `idProducto` int(11) NOT NULL,
-  `cantidad` int(3) NOT NULL,
-  `medida` varchar(2) NOT NULL,
-  `grasasTotales` int(3) NOT NULL,
-  `grasasSaturadas` int(3) NOT NULL,
-  `grasasMonoInsaturadas` int(3) NOT NULL,
-  `grasasPoliInsaturadas` int(3) NOT NULL,
-  `hidratosCarbono` int(3) NOT NULL,
-  `azucares` int(3) NOT NULL,
-  `fibra` int(3) NOT NULL,
-  `sal` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `descripcion` varchar(300) NOT NULL,
-  `imagen` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nickName` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `avatar` varchar(30) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellidos` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `direcciones`
---
-ALTER TABLE `direcciones`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idUsuario` (`idUsuario`);
-
---
--- Indices de la tabla `informacionnutricional`
---
-ALTER TABLE `informacionnutricional`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idProducto` (`idProducto`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nickName` (`nickName`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `direcciones`
---
-ALTER TABLE `direcciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `informacionnutricional`
---
-ALTER TABLE `informacionnutricional`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `direcciones`
---
-ALTER TABLE `direcciones`
-  ADD CONSTRAINT `fk_direcciones_usuarios_id` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `informacionnutricional`
---
-ALTER TABLE `informacionnutricional`
-  ADD CONSTRAINT `fk_informacionnutricional_productos_id` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`id`) ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
