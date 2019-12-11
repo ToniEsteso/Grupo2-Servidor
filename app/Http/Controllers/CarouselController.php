db8f8f88557933cabb43de84e03c5b15e6e28f42
JOSE MIGUEL ZOMEÑO ESCOBAR <20499963v@AD.FPMISLATA.COM>
2019-12-11 13:53:09 +0100

    fix(#9):haciendo que se carguen las imagenes desde servidor

--- app/Http/Controllers/CarouselController.php
+++ app/Http/Controllers/CarouselController.php
@@ -6,21 +6,27 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Models\Carousel;
 use App\Http\Models\Mensaje;
 
 class CarouselController extends BaseController
 {
     function GetAll(){
-        $directorio = '/imagenes';
-        // $imagenes = scandir($directorio);
-        $imagenes = "";
+       /*  $mensajes = array("200" => "Todo correcto",
+                    "404" => "No encontrado"); */
+
+        // $directorio = opendir('/public/imagenes');
+        echo Storage::get('public_images');
+        $imagenes =  Storage::disk('public_images')->files();
+        //$imagenes = readdir($directorio);
+        var_dump($imagenes);
         // Si $imagenes no está vacio, retorna success. Si está vacio retornará error 404. DARLE UNA VUELTA
         if(empty($imagenes)){
-            $respuesta = new Mensaje(404, $imagenes);
-            var_dump($respuesta->respuesta["mensaje"]);
-        } /* else{
-        } */
+             $respuesta = null;
+        }  else{
+            $respuesta = $imagenes;
+        } 
 
         return $respuesta;
     }
