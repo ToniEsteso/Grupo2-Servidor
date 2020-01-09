474aa63d253d02c3eb458795088288b924cb369d
DAVID DEL RIO CATALAN <53259208v@AD.FPMISLATA.COM>
2020-01-09 13:29:50 +0100

    sql actualizado

--- sql/veganfood.sql
+++ sql/veganfood.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         localhost
+-- Host:                         127.0.0.1
 -- Versión del servidor:         10.4.6-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win64
--- HeidiSQL Versión:             10.3.0.5771
+-- HeidiSQL Versión:             10.2.0.5599
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -13,7 +13,7 @@
 
 
 -- Volcando estructura de base de datos para veganfood
-CREATE DATABASE IF NOT EXISTS `veganfood` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `veganfood` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `veganfood`;
 
 -- Volcando estructura para tabla veganfood.categorias
@@ -28,8 +28,8 @@ CREATE TABLE IF NOT EXISTS `categorias` (
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
 INSERT INTO `categorias` (`id`, `nombre`, `icono`) VALUES
 	(1, 'Carnes', 'fas fa-drumstick-bite'),
-	(4, 'Bebidas', 'fas fa-coffee'),
-	(5, 'Frutas', 'fas fa-apple-alt');
+	(2, 'Bebidas', 'fas fa-coffee'),
+	(3, 'Frutas', 'fas fa-apple-alt');
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 
 -- Volcando estructura para tabla veganfood.direcciones
@@ -96,18 +96,35 @@ CREATE TABLE IF NOT EXISTS `informacionnutricional` (
 CREATE TABLE IF NOT EXISTS `productos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
-  `categoria` varchar(40) NOT NULL,
   `precio` float(5,2) NOT NULL,
   `descripcion` varchar(300) NOT NULL,
   `imagen` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla veganfood.productos: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla veganfood.productos: ~20 rows (aproximadamente)
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` (`id`, `nombre`, `categoria`, `precio`, `descripcion`, `imagen`) VALUES
-	(1, 'pechuga', '', 0.00, 'pechugota de carne autentica', 'imagen'),
-	(2, 'entrecot', '', 0.00, 'entrecot que ha salido de matar a muchos animales', 'muerte');
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `descripcion`, `imagen`) VALUES
+	(1, 'Pechuga de Pavo', 3.00, 'Pechuga de pavo sin grasa', 'pechugaPavo.jpg'),
+	(2, 'Pechuga de Pollo', 5.00, 'Pechuga de pollo casi sin grasa', 'pechugaPollo.jpg'),
+	(3, 'Lomo de Cerdo', 4.00, 'Excelente lomo de cerdo para comer en plato o bocadillo', 'lomoCerdo.jpg'),
+	(4, 'Ternera Gallega', 7.00, 'Sabrosisimos filetes de ternera gallega', 'terneraGallega.jpg'),
+	(5, 'Entrecot de Vaca', 9.00, 'Jugoso entrecot de vaca, alimentada con lagrimas de veganos', 'entrecotVaca.jpg'),
+	(6, 'Agua Mineral', 0.50, 'Agua mineral sin sabor', 'aguaMineral.jpg'),
+	(7, 'Coca-Cola', 1.00, 'Coca-Cola Original con medio kg de azucar por lata', 'cocacola.jpg'),
+	(8, 'Zumo de Naranja', 1.50, 'Zumo de naranja natural con pulpa', 'zumoNaranja.jpg'),
+	(9, 'Leche Entera', 1.00, 'Leche entera de vaca asturiana', 'lecheEntera.jpg'),
+	(10, 'Aquarius', 2.00, 'Posiblemente el peor refresco ever', 'aquarius.jpg'),
+	(11, 'Platano', 1.00, 'Platanos de canarias', 'platano.jpg'),
+	(12, 'Manzana', 1.00, 'Manzanas del eden', 'manzana.jpg'),
+	(13, 'Pera', 1.00, 'Pera clasicas de toda la vida', 'pera,jpg'),
+	(14, 'Melocoton', 1.00, 'Melocotones robados por gitanos', 'melocoton.jpg'),
+	(15, 'Uva', 1.00, 'Uva rancia y con semillas para nochevieja', 'uva,jpg'),
+	(16, 'Sandia', 1.00, 'Sandia de la que comen los niggas', 'sandia.jpg'),
+	(17, 'Melon', 1.00, 'Melon de albacete bueno bueno', 'melon.jpg'),
+	(18, 'Kiwi', 1.00, 'Kiwi de los que hacen que no cagues', 'kiwi.jpg'),
+	(19, 'Naranja', 1.00, 'Naranjas de valencia', 'naranja.jpg'),
+	(20, 'Mandarina', 1.00, 'Son como naranjas pero con enanismo', 'mandarinas.jpg');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
 -- Volcando estructura para tabla veganfood.productos_categorias
@@ -120,11 +137,29 @@ CREATE TABLE IF NOT EXISTS `productos_categorias` (
   CONSTRAINT `fk_productos_categorias2` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla veganfood.productos_categorias: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla veganfood.productos_categorias: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `productos_categorias` DISABLE KEYS */;
 INSERT INTO `productos_categorias` (`idProducto`, `idCategoria`) VALUES
 	(1, 1),
-	(2, 1);
+	(2, 1),
+	(3, 1),
+	(4, 1),
+	(5, 1),
+	(6, 2),
+	(7, 2),
+	(8, 2),
+	(9, 2),
+	(10, 2),
+	(11, 3),
+	(12, 3),
+	(13, 3),
+	(14, 3),
+	(15, 3),
+	(16, 3),
+	(17, 3),
+	(18, 3),
+	(19, 3),
+	(20, 3);
 /*!40000 ALTER TABLE `productos_categorias` ENABLE KEYS */;
 
 -- Volcando estructura para tabla veganfood.redessociales
