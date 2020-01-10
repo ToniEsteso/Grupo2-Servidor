0bbbeced558b440a84076909b4859a474cdc0e93
David <daviddelriocatalan@gmail.com>
2020-01-10 17:41:04 +0100

    RedesSocialesController actualizado

--- app/Http/Controllers/RedesSocialesController.php
+++ app/Http/Controllers/RedesSocialesController.php
@@ -11,18 +11,24 @@ use App\Http\Models\RedesSociales;
 class RedesSocialesController extends BaseController
 {
     function GetAll(){
-        $respuesta = array(
-            "mensaje" => "Todos las redes sociales",
-            "data" => RedesSociales::get()
-        );
+        $redesSociales = RedesSociales::get();
+
+        if(empty($redesSociales)){
+            $respuesta = config('codigosRespuesta.404');
+        } else{
+            $respuesta = array("mensaje" => config('codigosRespuesta.200'), "data" => $redesSociales);
+        }
 
         return $respuesta;
     }
     function Get($redSocial){
-        $respuesta = array(
-            "mensaje" => "Toda la informacion de" . $redSocial,
-            "data" => RedesSociales::Where("nombre", $redSocial)->get()
-        );
+        $rs = RedesSociales::Where("nombre", $redSocial)->get();
+
+        if(empty($rs)){
+            $respuesta = config('codigosRespuesta.404');
+        } else{
+            $respuesta = array("mensaje" => config('codigosRespuesta.200'), "data" => $rs);
+        }
 
         return $respuesta;
     }
