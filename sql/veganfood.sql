c5e76feecdf46d0005386a9ebad3aa473277efc1
Toni Esteso <toniesteso97@gmail.com>
2020-01-15 13:34:36 +0100

    sql actualizado

--- sql/veganfood.sql
+++ sql/veganfood.sql
@@ -162,25 +162,6 @@ INSERT INTO `productos_categorias` (`idProducto`, `idCategoria`) VALUES
 	(20, 3);
 /*!40000 ALTER TABLE `productos_categorias` ENABLE KEYS */;
 
--- Volcando estructura para tabla veganfood.recetas
-CREATE TABLE IF NOT EXISTS `recetas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) DEFAULT NULL,
-  `imagen` varchar(100) DEFAULT NULL,
-  `enlace` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
--- Volcando datos para la tabla veganfood.recetas: ~5 rows (aproximadamente)
-/*!40000 ALTER TABLE `recetas` DISABLE KEYS */;
-INSERT INTO `recetas` (`id`, `nombre`, `imagen`, `enlace`) VALUES
-	(1, 'Berenjenas Rellenas', 'berenjenasrellenas.jpg', 'https://www.hogarmania.com/cocina/recetas/ensaladas-verduras/201802/berenjena-rellena-arroz-verduras-salsa-39060.html'),
-	(2, 'Tomates Rellenos', 'tomatesrellenos.jpg', 'https://www.cocina-familiar.com/tomates-rellenos-con-ensaladilla-de-arroz.html'),
-	(3, 'Ensalada Valenciana', 'ensaladavalenciana.jpg', 'http://www.recetasparainutiles.com/recetas/vegetales/ensalada-valenciana'),
-	(4, 'Hamburguesa Vegana', 'hamburguesavegana.jpg', 'https://delantaldealces.com/hamburguesa-vegana-garbanzos-tofu/'),
-	(5, 'Sopa de Verduras', 'sopaverduras.jpg', 'https://cocina-casera.com/mx/sopa-verduras-receta-mexicana-facil/');
-/*!40000 ALTER TABLE `recetas` ENABLE KEYS */;
-
 -- Volcando estructura para tabla veganfood.redessociales
 CREATE TABLE IF NOT EXISTS `redessociales` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -204,16 +185,19 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `nickName` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(256) NOT NULL,
-  `avatar` varchar(30) NOT NULL,
+  `avatar` varchar(256) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
+  `admin` int(11) DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nickName` (`nickName`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla veganfood.usuarios: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla veganfood.usuarios: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` (`id`, `nickName`, `email`, `password`, `avatar`, `nombre`, `apellidos`, `admin`) VALUES
+	(26, 'Toni', 'toniesteso97@gmail.com', '$2y$10$yWbUHPliaAesY3zzp8aSP.Lp66GnWCSivc0rKkIHwqPOAiEdI0eUC', 'Toni.jpeg', 'Toni', 'Toni', 0);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
